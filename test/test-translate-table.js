91cf9a138035643bcdb77cf5ae2c13f9df1829ce
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2015-09-21 14:52:34 +0300

    [issue-2] Fixed problems with softening

--- test/test-translate-table.js
+++ test/test-translate-table.js
@@ -4,12 +4,35 @@ var table = JSON.parse(data.load('../data/translate-table.json'));
 var transliterateText = function transliterate(text, transTable) {
   var outputText = '';
 
+  var check3 = function check3(index) {
+    if ((i < text.length - 2) &&
+        (transTable.hasOwnProperty(text[i] + text[i+1] + text[i+2]))) {
+      return true;
+    }
+  };
+
+  var check2 = function check2(index) {
+    if ((i < text.length - 1) &&
+        (transTable.hasOwnProperty(text[i] + text[i+1]))) {
+      return true;
+    }
+  };
+
+  var check1 = function check1(index) {
+    if (transTable.hasOwnProperty(text[index])) {
+      return true;
+    }
+  };
+
   var i = 0;
   while (i < text.length) {
-    if ((i < text.length - 1) && (transTable.hasOwnProperty(text[i] + text[i+1]))) {
+    if (check3(i)) {
+      outputText += transTable[text[i] + text[i+1] + text[i+2]];
+      i += 2;
+    } else if (check2(i)) {
       outputText += transTable[text[i] + text[i+1]];
       i++;
-    } else if (transTable.hasOwnProperty(text[i])) {
+    } else if (check1(i)) {
       outputText += transTable[text[i]];
     } else {
       outputText += text[i];
@@ -29,6 +52,8 @@ exports["test translate-table hajevycja"] = function(assert) {
   assert.equal(transliterateText("", curTable), "");
   assert.equal(transliterateText("землі", curTable), "zemli");
   assert.equal(transliterateText("культурні", curTable), "kuljturni");
+  assert.equal(transliterateText("для осмислення всього цього", curTable),
+               "dlja osmyslennja vsjoho cjoho");
   assert.equal(transliterateText("м'ясо", curTable), "m'jaso");
 };
 
@@ -39,7 +64,7 @@ exports["test translate-table jirechekivka"] = function(assert) {
   assert.equal(transliterateText("землі", curTable), "zemli");
   assert.equal(transliterateText("культурні", curTable), "kuľturni");
   assert.equal(transliterateText("для осмислення всього цього", curTable),
-               "dlia osmyslennia vśoho ćoho");
+               "dlia osmyslennia vsioho cioho");
   assert.equal(transliterateText("м'ясо", curTable), "mjaso");
 };
 
@@ -49,6 +74,8 @@ exports["test translate-table lahodivka"] = function(assert) {
   assert.equal(transliterateText("", curTable), "");
   assert.equal(transliterateText("землі", curTable), "zemli");
   assert.equal(transliterateText("культурні", curTable), "kuľturni");
+  assert.equal(transliterateText("для осмислення всього цього", curTable),
+               "dľa osmyslenńa vśoho ćoho");
   assert.equal(transliterateText("м'ясо", curTable), "mjaso");
 };
 
@@ -58,6 +85,8 @@ exports["test translate-table luchukivka"] = function(assert) {
   assert.equal(transliterateText("", curTable), "");
   assert.equal(transliterateText("землі", curTable), "zemli");
   assert.equal(transliterateText("культурні", curTable), "kuľturni");
+  assert.equal(transliterateText("для осмислення всього цього", curTable),
+               "dľa osmyslenńa vśoho ćoho");
   assert.equal(transliterateText("м'ясо", curTable), "mjaso");
 };
 
